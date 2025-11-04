4d239553690b4b3eb4eda3e8bca4ff3c935ae3ea
Álvaro Braz <alvarobraz83@gmail.com>
2025-11-04 14:41:12 -0300

    feat: layout structure - mock photo list

--- src/pages/page-home.tsx
+++ src/pages/page-home.tsx
@@ -1,12 +1,12 @@
 import Container from "../components/container";
-import PhotoWidget from "../contexts/photos/components/photo-widget";
+import PhotosList from "../contexts/photos/components/photos-list";
 
 export default function PageHome() {
   return (
     <Container>
-      <div className="grid grid-cols-4 gap-9">
-        <PhotoWidget
-          photo={{
+      <PhotosList
+        photos={[
+          {
             id: "123",
             title: "Olá mundo!",
             imageId: "portrait-tower.png",
@@ -15,22 +15,8 @@ export default function PageHome() {
               { id: "123", title: "Album 2" },
               { id: "456", title: "Album 3" },
             ],
-          }}
-        />
-        <PhotoWidget
-          photo={{
-            id: "123",
-            title: "Olá gente!",
-            imageId: "portrait-tower.png",
-            albums: [
-              { id: "123", title: "Album 1" },
-              { id: "456", title: "Album 2" },
-              { id: "789", title: "Album 3" },
-            ],
-          }}
-        />
-        <PhotoWidget
-          photo={{
+          },
+          {
             id: "123",
             title: "Olá mundo!",
             imageId: "portrait-tower.png",
@@ -38,11 +24,40 @@ export default function PageHome() {
               { id: "3421", title: "Album 1" },
               { id: "123", title: "Album 2" },
               { id: "456", title: "Album 3" },
+            ],
+          },
+          {
+            id: "123",
+            title: "Olá mundo!",
+            imageId: "portrait-tower.png",
+            albums: [
+              { id: "3421", title: "Album 1" },
+              { id: "123", title: "Album 2" },
               { id: "456", title: "Album 3" },
             ],
-          }}
-        />
-      </div>
+          },
+          {
+            id: "123",
+            title: "Olá mundo!",
+            imageId: "portrait-tower.png",
+            albums: [
+              { id: "3421", title: "Album 1" },
+              { id: "123", title: "Album 2" },
+              { id: "456", title: "Album 3" },
+            ],
+          },
+          {
+            id: "123",
+            title: "Olá mundo!",
+            imageId: "portrait-tower.png",
+            albums: [
+              { id: "3421", title: "Album 1" },
+              { id: "123", title: "Album 2" },
+              { id: "456", title: "Album 3" },
+            ],
+          },
+        ]}
+      />
     </Container>
   );
 }
