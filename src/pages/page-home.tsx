c450cb0e32f26d3921e4107ed84ac7aaffc6eec4
Álvaro Braz <alvarobraz83@gmail.com>
2025-11-04 14:16:22 -0300

    feat: layout structure - photo widget component

--- src/pages/page-home.tsx
+++ src/pages/page-home.tsx
@@ -1,9 +1,48 @@
-import Text from "../components/text";
+import Container from "../components/container";
+import PhotoWidget from "../contexts/photos/components/photo-widget";
 
 export default function PageHome() {
   return (
-    <>
-      <Text variant="heading-medium">Página inicial</Text>
-    </>
+    <Container>
+      <div className="grid grid-cols-4 gap-9">
+        <PhotoWidget
+          photo={{
+            id: "123",
+            title: "Olá mundo!",
+            imageId: "portrait-tower.png",
+            albums: [
+              { id: "3421", title: "Album 1" },
+              { id: "123", title: "Album 2" },
+              { id: "456", title: "Album 3" },
+            ],
+          }}
+        />
+        <PhotoWidget
+          photo={{
+            id: "123",
+            title: "Olá gente!",
+            imageId: "portrait-tower.png",
+            albums: [
+              { id: "123", title: "Album 1" },
+              { id: "456", title: "Album 2" },
+              { id: "789", title: "Album 3" },
+            ],
+          }}
+        />
+        <PhotoWidget
+          photo={{
+            id: "123",
+            title: "Olá mundo!",
+            imageId: "portrait-tower.png",
+            albums: [
+              { id: "3421", title: "Album 1" },
+              { id: "123", title: "Album 2" },
+              { id: "456", title: "Album 3" },
+              { id: "456", title: "Album 3" },
+            ],
+          }}
+        />
+      </div>
+    </Container>
   );
 }
